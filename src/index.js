dbc16f5decb15f73ce39d670f8a0a61075160b3c
jackyrwj <raowenjieszu@foxmail.com>
2024-08-01 08:25:08 +0800

    🔧fix configuration

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ export const maxHour = 12;                 // 报表最大小时数
 export const urlspath = "/src/urls.cfg";  // 配置文件路径,不带后/
 export const logspath = "./logs";          // 日志文件路径,不带后/
 export const reloadReportsdata = false;     // 是否重新加载报告
-export const reloadReportstime = 2.5;        // 重载报告的检测间隔时间
+export const reloadReportstime = 5;        // 重载报告的检测间隔时间
 
 
 // 主函数，异步执行一系列操作。
